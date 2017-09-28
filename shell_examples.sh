f17fff257b1390c833a28dff38526ce203a23ac8
takuzuka@mail.ru <takuzuka@mail.ru>
2017-09-28 09:51:22 +0300

    20170929_09_50

---
+++ shell_examples.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#5.piemērs
+echo "Atbilde uz jautajumu - kas shobrid tiek pildīts?" $0
+#echo $n
+echo "Skriptam nodoto argumentu skaits: " $#
+
+
+#4.piemērs
+#echo $0
+
+#3.piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
+#unset NAME
+#echo $NAME
+
+#2.piemērs
+#NAME="Vārds Uzvārds"
+#readonly NAME
+#echo $NAME
+#NAME="Uzvāŗds Vards"
+#echo $NAME
+
+#1.piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
+
